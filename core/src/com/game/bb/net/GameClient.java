4ae2a2c545f9b3ee185ac81b70396bafd7f885ce
Oskar Holmberg <oskarholmberg@msn.com>
2016-05-11 11:48:50 +0200

    Added disconnecter hook to let server know when a client is shut down.

--- core/src/com/game/bb/net/GameClient.java
+++ core/src/com/game/bb/net/GameClient.java
@@ -11,6 +11,8 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
+import javax.xml.crypto.Data;
+
 /**
  * Created by oskar on 5/11/16.
  */
@@ -20,10 +22,12 @@ public class GameClient extends Thread {
     private InetAddress ipAddress;
     private DatagramSocket socket;
     private int port;
+    private Disconnecter disconnecter;
 
     public GameClient(GameStateManager gsm, String ipAddress, int port){
         this.gsm=gsm;
         this.port=port;
+        disconnecter = new Disconnecter(this);
         try {
             System.out.println("Connecting to " + ipAddress + ":" + port);
             this.socket=new DatagramSocket();
@@ -57,6 +61,28 @@ public class GameClient extends Thread {
             socket.send(packet);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    protected void disconnect(){
+        byte[] disconnect = "disconnect".getBytes();
+        sendData(disconnect);
+    }
+
+    public Runnable getDisconnecter(){
+        return disconnecter;
+    }
+
+    private static class Disconnecter implements Runnable{
+
+        private GameClient client;
+
+        private Disconnecter(GameClient client){
+            this.client = client;
+        }
+        @Override
+        public void run() {
+            client.disconnect();
         }
     }
 }
