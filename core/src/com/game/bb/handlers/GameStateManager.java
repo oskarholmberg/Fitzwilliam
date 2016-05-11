d85e77b9683149b0470044e5ea30acee78687144
Oskar Holmberg <oskarholmberg@msn.com>
2016-05-11 20:58:57 +0200

    Started setup protocol implementation

--- core/src/com/game/bb/handlers/GameStateManager.java
+++ core/src/com/game/bb/handlers/GameStateManager.java
@@ -3,6 +3,7 @@ package com.game.bb.handlers;
 import com.game.bb.main.Game;
 import com.game.bb.gamestates.GameState;
 import com.game.bb.net.GameClient;
+import com.game.bb.net.GameServer;
 
 import java.util.ArrayList;
 import java.util.Stack;
@@ -16,16 +17,32 @@ public class GameStateManager {
     private int PLAY = 123123;
     private ArrayList<String> opponentActions;
     private GameClient client;
+    private String ipAddress;
+    private int port;
+    private boolean host;
 
 
     public GameStateManager(Game game, String ipAddress, int port) {
         this.game = game;
+        this.ipAddress = ipAddress;
+        this.port = port;
         states = new Stack<GameState>();
         opponentActions = new ArrayList<String>();
+        init();
+        pushState(PLAY);
+    }
+
+    private void init(){
+        //If host start a server
+        if(host){
+            new GameServer(port).start();
+        }
+        //Start client
         client = new GameClient(this, ipAddress, port);
         client.start();
         Runtime.getRuntime().addShutdownHook(new Thread(client.getDisconnecter()));
-        pushState(PLAY);
+        byte[] clientInfo = "SETUP".getBytes();
+        client.sendData(clientInfo);
     }
 
     public Game game() {
