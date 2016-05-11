ec1fb35ef03698f4b384e297c28ef05e5cded212
Oskar Holmberg <oskarholmberg@msn.com>
2016-05-11 12:53:52 +0200

    Add sounds and textures

--- core/src/com/game/bb/entities/SPPlayer.java
+++ core/src/com/game/bb/entities/SPPlayer.java
@@ -1,5 +1,7 @@
 package com.game.bb.entities;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.physics.box2d.Body;
 
 /**
@@ -7,14 +9,19 @@ import com.badlogic.gdx.physics.box2d.Body;
  */
 public class SPPlayer extends SPSprite {
 
-    public SPPlayer(Body[] bodies) {
+    Sound sound;
+
+    public SPPlayer(Body[] bodies)
+    {
         super(bodies[0]);
+        sound = Gdx.audio.newSound(Gdx.files.internal("sfx/jump.wav"));
     }
 
     public void movePaddle(float xForce, float yForce, float xPos, float yPos){
         body.setTransform(xPos, yPos, 0);
         body.setLinearVelocity(0, 0);
         body.applyForceToCenter(xForce, yForce, true);
+        sound.play();
     }
 
 
