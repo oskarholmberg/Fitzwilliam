ff9182f5205719f7958176ce99a997de3197f833
erikmunkby <erik.munkby@gmail.com>
2016-05-15 17:51:37 +0200

    changed float timediff to long

--- core/src/com/game/bb/gamestates/PlayState.java
+++ core/src/com/game/bb/gamestates/PlayState.java
@@ -85,7 +85,7 @@ public class PlayState extends GameState {
             Vector2 pos = player.getPosition();
             SPBullet bullet = new SPBullet(world, pos.x, pos.y, lastJumpDirection, false);
             bullets.add(bullet);
-            mon.sendPlayerAction("SHOOT", 0, 0, Float.toString(lastJumpDirection), Float.toString(System.currentTimeMillis()));
+            mon.sendPlayerAction("SHOOT", 0, 0, Float.toString(lastJumpDirection), Long.toString(System.currentTimeMillis()));
             amntBullets--;
             hud.setAmountBulletsLeft(amntBullets);
             if (amntBullets == 0) {
@@ -95,9 +95,11 @@ public class PlayState extends GameState {
         }
     }
 
-    public void opponentShot(float xPos, float yPos, float dir, float timeSync) {
-        float timeDiff = System.currentTimeMillis() - timeSync;
-        xPos = xPos + (B2DVars.PH_BULLET_SPEED * timeDiff/1000) / B2DVars.PPM;
+    public void opponentShot(float xPos, float yPos, float dir, long timeSync) {
+        long timeDiff = System.currentTimeMillis() - timeSync;
+        System.out.println("TimeDiff: " + timeDiff + " previous xPos: " + xPos);
+        xPos = xPos + (B2DVars.PH_BULLET_SPEED * timeDiff/1000) / B2DVars.PPM * dir;
+        System.out.println("Time synced xPos: " + xPos);
         SPBullet bullet = new SPBullet(world, xPos, yPos, dir, true);
         bullets.add(bullet);
     }
@@ -138,7 +140,7 @@ public class PlayState extends GameState {
                 opponent.jump(floats[0], floats[1],
                         floats[2], floats[3]);
             else if (action[1].equals("SHOOT") && opponent != null)
-                opponentShot(floats[2], floats[3], Float.valueOf(action[6]), Float.valueOf(action[7]));
+                opponentShot(floats[2], floats[3], Float.valueOf(action[6]), Long.valueOf(action[7]));
             else if (action[1].equals("DEATH") && opponent != null) {
                 hud.setOpponentDeath(action[0], action[6]);
                 opponent.kill(1);
