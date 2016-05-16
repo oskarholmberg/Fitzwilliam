bf1881395c0848c4782877cdfa0b901b76a709e5
erikmunkby <erik.munkby@gmail.com>
2016-05-16 22:24:26 +0200

    tjoho

--- core/src/com/game/bb/gamestates/PlayState.java
+++ core/src/com/game/bb/gamestates/PlayState.java
@@ -325,10 +325,11 @@ public class PlayState extends GameState {
             SPSprite temp = (SPSprite) cl.getKillingEntity().getBody().getUserData();
             if (temp instanceof SPGrenade){
                 world.destroyBody(grenades.remove(temp.getID()).getBody());
+            } else {
+                //In this addAction add the ID of the killing bullet last
+                mon.sendPlayerAction("DEATH", 0, 0, hud.getDeathCount(),
+                        cl.getKillingEntity().getID(), Float.toString(cl.getKillingEntity().getDirection()));
             }
-            //In this addAction add the ID of the killing bullet last
-            mon.sendPlayerAction("DEATH", 0, 0, hud.getDeathCount(),
-                    cl.getKillingEntity().getID(), Float.toString(cl.getKillingEntity().getDirection()));
         }
     }
 
