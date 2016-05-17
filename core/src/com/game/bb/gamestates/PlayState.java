af74b65ea30a8eaeee79e672ab45ee39d1138815
erikmunkby <erik.munkby@gmail.com>
2016-05-17 19:13:57 +0200

    killing bullets should now also be removed

--- core/src/com/game/bb/gamestates/PlayState.java
+++ core/src/com/game/bb/gamestates/PlayState.java
@@ -325,9 +325,14 @@ public class PlayState extends GameState {
             hud.addPlayerDeath();
             SPSprite temp = (SPSprite) cl.getKillingEntity().getBody().getUserData();
             if (temp instanceof SPGrenade){
-                world.destroyBody(grenades.removeKey(temp.getID()).getBody());
                 mon.sendPlayerAction("DEATH", 0, 0, hud.getDeathCount(),
                         cl.getKillingEntity().getID(), Float.toString(cl.getKillingEntity().getDirection()));
+                world.destroyBody(grenades.removeKey(temp.getID()).getBody());
+            } else if (temp instanceof SPBullet){
+                mon.sendPlayerAction("DEATH", 0, 0, hud.getDeathCount(),
+                        cl.getKillingEntity().getID(), Float.toString(cl.getKillingEntity().getDirection()));
+                world.destroyBody(temp.getBody());
+                worldEntities.removeValue(temp, true);
             }
         }
     }
