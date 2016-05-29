028ef3f1c460b7a0a711a8d2db1e0d943ea39b2a
oskarholmberg <dat12oho@student.lu.se>
2016-05-29 16:20:26 +0200

    Fixed race condition when taking powerups

--- core/src/com/game/bb/gamestates/PlayState.java
+++ core/src/com/game/bb/gamestates/PlayState.java
@@ -292,9 +292,9 @@ public class PlayState extends GameState {
                 if (pkt.misc == B2DVars.POWERTYPE_TILTSCREEN) {
                     powerHandler.applyPowerup(pkt.misc);
                 } else if (pkt.misc == B2DVars.POWERTYPE_SHIELD) {
-                    if (pkt.miscString.equals("applyShield")){
+                    if (pkt.miscString.equals("applyShield")) {
                         opponents.get(pkt.id).applyShield();
-                    } else if (pkt.miscString.equals("removeShield")){
+                    } else if (pkt.miscString.equals("removeShield")) {
                         opponents.get(pkt.id).removeShield();
                     }
                 }
@@ -561,35 +561,36 @@ public class PlayState extends GameState {
             gsm.setState(GameStateManager.HOST_OFFLINE);
         }
         world.step(dt, 6, 2);
-        if (player != null)
+        if (player != null) {
             player.update(dt);
-        for (IntMap.Keys it = opponents.keys(); it.hasNext; ) {
-            opponents.get(it.next()).update(dt);
-        }
-        for (IntMap.Keys it = myEntities.keys(); it.hasNext; ) {
-            myEntities.get(it.next()).update(dt);
-        }
-        for (IntMap.Keys it = opEntities.keys(); it.hasNext; ) {
-            opEntities.get(it.next()).update(dt);
-        }
-        for (IntMap.Keys it = powerups.keys(); it.hasNext; ) {
-            powerups.get(it.next()).update(dt);
-        }
-        opponentTCPEvents();
-        opponentEntityEvents();
-        opponentMovementEvents();
+            for (IntMap.Keys it = opponents.keys(); it.hasNext; ) {
+                opponents.get(it.next()).update(dt);
+            }
+            for (IntMap.Keys it = myEntities.keys(); it.hasNext; ) {
+                myEntities.get(it.next()).update(dt);
+            }
+            for (IntMap.Keys it = opEntities.keys(); it.hasNext; ) {
+                opEntities.get(it.next()).update(dt);
+            }
+            for (IntMap.Keys it = powerups.keys(); it.hasNext; ) {
+                powerups.get(it.next()).update(dt);
+            }
+            opponentTCPEvents();
+            opponentEntityEvents();
+            opponentMovementEvents();
 
-        refreshAmmo(dt);
-        if (cl.isPlayerHit()) {
-            playerHit();
-        }
-        if (cl.bouncePlayer()) {
-            player.bouncePlayer();
-        }
-        if (player.isDead()) {
-            respawnTimer += dt;
-            if (respawnTimer >= B2DVars.RESPAWN_TIME) {
-                respawnPlayer();
+            refreshAmmo(dt);
+            if (cl.isPlayerHit()) {
+                playerHit();
+            }
+            if (cl.bouncePlayer()) {
+                player.bouncePlayer();
+            }
+            if (player.isDead()) {
+                respawnTimer += dt;
+                if (respawnTimer >= B2DVars.RESPAWN_TIME) {
+                    respawnPlayer();
+                }
             }
         }
         if (sendEntityInfo > B2DVars.ENTITY_UPDATE_FREQ) {
