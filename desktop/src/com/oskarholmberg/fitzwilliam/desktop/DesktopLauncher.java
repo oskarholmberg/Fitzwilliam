c9bda0978f079fa89db18ed14504bbe088def093
oskarholmberg <dat12oho@student.lu.se>
2016-07-24 02:01:30 +0200

    Fixed desktop launcher to apply proper screen size

--- desktop/src/com/oskarholmberg/fitzwilliam/desktop/DesktopLauncher.java
+++ desktop/src/com/oskarholmberg/fitzwilliam/desktop/DesktopLauncher.java
@@ -2,11 +2,17 @@ package com.oskarholmberg.fitzwilliam.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.oskarholmberg.fitzwilliam.handlers.B2DVars;
 import com.oskarholmberg.fitzwilliam.main.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title= Game.TITLE;
+		config.width= B2DVars.DEFAULT_GAME_WIDTH;
+		config.height= B2DVars.DEFAULT_GAME_HEIGHT;
+
 		new LwjglApplication(new Game(), config);
 	}
 }
